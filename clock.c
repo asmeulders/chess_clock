eaaa0ab8116543db0c3de49ead4f57f71ee19753
Alexander Smeulders <absmeulders@gmail.com>
2026-05-12 22:04:24 -0400

    players outline done

--- clock.c
+++ clock.c
@@ -1,27 +1,11 @@
-#include <stdbool.h>
+#define _POSIX_C_SOURCE 199309L
 #include <time.h>
 #include <stdio.h>
 
-#include "players.h"
+void start_clock() {
 
-struct Player {
-  bool is_active;
-  time_t time_remaining;
-};
-
-Player* create_player(int game_duration) {
-  time_t duration = ;
-  Player *p = {false, duration};
 }
 
-bool is_active(Player *p) {
-  return p->is_active;
-}
-
-bool is_eliminated(Player *p) {
-  return p->time_remaining <= 0;
-}
-
-void change_turns(Player *p1, Player *p2) {
-
+void stop_clock() {
+  
 }
---
+++ players.c
@@ -0,0 +1,33 @@
+#define _POSIX_C_SOURCE 199309L
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+
+#include "players.h"
+
+struct Player {
+    struct timespec time_remaining;
+    bool is_active;
+};
+
+Player* create_player(int game_duration) {
+    struct timespec duration = { .tv_sec = game_duration, .tv_nsec = 0 };
+    Player *p = {false, duration};
+}
+
+bool is_active(Player *p) {
+    return p->is_active;
+}
+
+bool is_eliminated(Player *p) {
+    return p->time_remaining.tv_sec <= 0 && p->time_remaining.tv_nsec <= 0;
+}
+
+void change_turns(Player *p1, Player *p2) {
+    if (p1->is_active != p2->is_active) {
+        p1->is_active = !p1->is_active;
+        p2->is_active = !p2->is_active;
+    } else {
+        printf("Game has not started yet.");
+    }
+}
